be6f602b34b20bd5a8ab9a3d0b802341604d3e51
Gabriel Speer <gspeer2@ut.utm.edu>
2023-09-07 17:04:06 -0500

    while loop for sum

--- main.cpp
+++ main.cpp
@@ -3,7 +3,23 @@
 
 using namespace std;
 
+int sum(int num);
+
 int main() {
-  cout<<"Hello World!"<<endl;    
+  cout<<"Enter a whole number: "<<endl;
+  int num=0;
+  cin>>num;
+  cout<<sum(num)<<endl;
   return 0;
+}
+
+
+int sum(int num){
+  int count=0;
+  int value=0;
+  while(count<=num){
+    value+=count;
+    count++;
+  }
+  return value;
 }
